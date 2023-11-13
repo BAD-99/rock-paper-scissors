41bf88212d8adb1d9807294618a5a8af2b0cb815
BAD-99 <brandon.andrew.davis@gmail.com>
2023-11-13 02:03:53 -0700

    Added buttons to page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-//get player input
-//make a random choice
-//calculate results
-var playerPoints = 0
-var computerPoints = 0
+const results = document.querySelector('.results');
+
+var playerPoints = 0;
+var computerPoints = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button)=>button.addEventListener("click", () => playRound(button.innerText)))
 
 function game(){
-    playerPoints = 0
+    /* playerPoints = 0
     computerPoints = 0
     while(playerPoints < 3 && computerPoints < 3){
         console.log(playRound(prompt("Rock, paper, or scissors?")))
@@ -15,40 +17,54 @@ function game(){
     }
     else{
         console.log(`Computer wins!`)
-    }
+    } */
 }
 
-function playRound(playerInput){
+function choiceToInt(input){
     //sanitize player input
-    const choiceArr = ['rock', 'paper', 'scissors']
-    let playerChoice = playerInput.toLowerCase()
-    switch(playerChoice){
+
+    let choice = input.toLowerCase()
+    switch(choice){
         case 'rock':
-            playerChoice = 0
+            choice = 0
             break
         case 'paper':
-            playerChoice = 1
+            choice = 1
             break
         case 'scissors':
-            playerChoice = 2
+            choice = 2
             break
-        default:
-            return `${playerInput} is not a valid choice`
     }
-    //check against computer
+    return choice;
+}
+var tries = 0;
+function playRound(playerInput){
+    
+    if(playerPoints >= 5 || computerPoints >= 5){return;}
+    const choiceArr = ['rock', 'paper', 'scissors']
+    let playerChoice = choiceToInt(playerInput);
     let computerChoice = getComputerChoice()
+    //check against computer
     if(playerChoice == computerChoice){
-        return `Draw! ${capitalizeFirst(choiceArr[playerChoice])} ties!`
+        updateResults( `Draw! ${capitalizeFirst(choiceArr[playerChoice])} ties!`);
     }
     if(playerChoice == (computerChoice + 1)%3){
         playerPoints++
-        return `You win! ${capitalizeFirst(choiceArr[playerChoice])} beats ${choiceArr[computerChoice]}!`
+        updateResults(`You win! ${capitalizeFirst(choiceArr[playerChoice])} beats ${choiceArr[computerChoice]}!`)
     }
     if(playerChoice == (computerChoice + 2)%3)
     {
         computerPoints++
-        return `You lose! ${capitalizeFirst(choiceArr[computerChoice])} beats ${choiceArr[playerChoice]}!`
+        updateResults(`You lose! ${capitalizeFirst(choiceArr[computerChoice])} beats ${choiceArr[playerChoice]}!`)
     }
+    console.log(tries++ + ', ' + playerPoints + ', ' + computerPoints);
+}
+
+function updateResults(str){
+    if(playerPoints>=5){results.firstElementChild.innerText = 'Player wins!';}
+    else if(computerPoints>=5){results.firstElementChild.innerText = 'Computer wins!';}
+    else{results.firstElementChild.innerText = str;}
+    results.lastElementChild.innerText = `Player Points: ${playerPoints} | Computer Points: ${computerPoints}`
 }
 
 function getComputerChoice(){
